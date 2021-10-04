f49bd3e7b3c88243ff1cc9c6b6594f0fc01314b8
io-qar <40149230+io-qar@users.noreply.github.com>
2021-10-04 14:45:10 +0300

    add is_solvable test

--- test_fifteen.py
+++ test_fifteen.py
@@ -1,5 +1,8 @@
 import pytest
 
+def test_is_solvable():
+	assert modelBoard.is_solvable() == True
+
 def test_info():
 	"""Метод, тестирующий корректный вывод информации о доске."""
 	assert modelBoard.info() == "Ваше поле:3 на 3, и имеет размер 80"
