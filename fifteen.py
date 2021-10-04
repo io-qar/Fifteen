ec3eb02963022d1da86e2fcace879c0fcfd162a5
io-qar <40149230+io-qar@users.noreply.github.com>
2021-10-04 14:57:09 +0300

    update tabs, add clicl manager

--- fifteen.py
+++ fifteen.py
@@ -9,12 +9,14 @@ def get_inv_count():
     inversions = 0
     inversion_board = board[:]
     inversion_board.remove(EMPTY_SQUARE)
+    
     for i in range(len(inversion_board)):
         first_item = inversion_board[i]
         for j in range(i+1, len(inversion_board)):
             second_item = inversion_board[j]
             if first_item > second_item:
                 inversions += 1
+                
     return inversions
 
 def is_solvable():
@@ -28,41 +30,22 @@ def is_solvable():
         else:
             return num_inversions % 2 == 0
 
-def get_empty_neighbor(index):
+def get_empty_neighbor(cl_index):
     empty_index = board.index(EMPTY_SQUARE)
-    abs_value = abs(empty_index - index)
+    abs_value = abs(empty_index - cl_index)
     if abs_value == BOARD_SIZE:
         return empty_index
     elif abs_value == 1:
-        max_index = max(index, empty_index)
+        max_index = max(cl_index, empty_index)
         if max_index % BOARD_SIZE != 0:
             return empty_index
-    return index
-
-def draw_board():
-    c.delete('all')
-   	for i in range(BOARD_SIZE):
-        for j in range(BOARD_SIZE):
-            index = str(board[BOARD_SIZE * i + j])
-            if index != str(EMPTY_SQUARE):
-                c.create_rectangle(
-			j * SQUARE_SIZE, i * SQUARE_SIZE,
-                    	j * SQUARE_SIZE + SQUARE_SIZE,
-                    	i * SQUARE_SIZE + SQUARE_SIZE,
-                    	fill = '#43ABC9',
-                    	outline = '#FFFFFF'
-		)
-                c.create_text(
-			j * SQUARE_SIZE + SQUARE_SIZE / 2,
-                    	i * SQUARE_SIZE + SQUARE_SIZE / 2,
-                    	text = index,
-                    	font = "Arial {} italic".format(int(SQUARE_SIZE / 4)),
-                    	fill = '#FFFFFF')
+        
+    return cl_index
 		
 def show_victory_plate():
     c.create_rectangle(
 	    	SQUARE_SIZE / 5,
-            	SQUARE_SIZE * BOARD_SIZE / 2 - 10 * BOARD_SIZE,
+            SQUARE_SIZE * BOARD_SIZE / 2 - 10 * BOARD_SIZE,
 	    	BOARD_SIZE * SQUARE_SIZE - SQUARE_SIZE / 5,
 	    	SQUARE_SIZE * BOARD_SIZE / 2 + 10 * BOARD_SIZE,
 	    	fill = '#000000',
@@ -70,7 +53,7 @@ def show_victory_plate():
     c.create_text(
 	    	SQUARE_SIZE * BOARD_SIZE / 2,
 	    	SQUARE_SIZE * BOARD_SIZE / 1.9,
-		text = "Victory!",
+		    text = "Victory!",
 	    	font = "Helvetica {} bold".format(int(10 * BOARD_SIZE)),
 	    	fill = '#DC143C'
     )
@@ -91,6 +74,40 @@ def show_error_plate(self):
 			font = "Helvetica {} bold".format(int(10 * BOARD_SIZE)),
 			fill = '#DC143C'
 		)
+        
+ def click(event):
+        x, y = event.x, event.y
+        
+        x //= SQUARE_SIZE
+        y //= SQUARE_SIZE
+        
+        board_index = x + (y * BOARD_SIZE)
+        empty_index = get_empty_neighbor(board_index)
+        board[board_index], board[empty_index] = board[empty_index], board[board_index]
+        draw_board()
+        if board == correct_board:
+            show_victory_plate()
+            
+def draw_board():
+    c.delete('all')
+   	for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            index = str(board[BOARD_SIZE * i + j])
+            if index != str(EMPTY_SQUARE):
+                c.create_rectangle(
+			        j * SQUARE_SIZE, i * SQUARE_SIZE,
+                    j * SQUARE_SIZE + SQUARE_SIZE,
+                    i * SQUARE_SIZE + SQUARE_SIZE,
+                    fill = '#43ABC9',
+                    outline = '#FFFFFF'
+		        )
+                c.create_text(
+			        j * SQUARE_SIZE + SQUARE_SIZE / 2,
+                    i * SQUARE_SIZE + SQUARE_SIZE / 2,
+                    text = index,
+                    font = "Arial {} italic".format(int(SQUARE_SIZE / 4)),
+                    fill = '#FFFFFF'
+                )
 
 class Model:
 	...
